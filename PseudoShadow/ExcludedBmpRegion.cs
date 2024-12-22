73ef4590198a3d4f044a8f3ac0bba1bdf97b541b
Thorsten Gudera <175501628+ThorstenGudera@users.noreply.github.com>
2024-12-22 07:28:16 +0100

    fixed a bug in ExcludedBmpRegion

--- PseudoShadow/ExcludedBmpRegion.cs
+++ PseudoShadow/ExcludedBmpRegion.cs
@@ -2,19 +2,20 @@
 namespace PseudoShadow
 {
     public class ExcludedBmpRegion : IDisposable
-    {   
-        public Bitmap? Remaining { get; }
+    {
+        public Bitmap? Remaining { get; set; }
         public Point Location { get; set; } = new Point(0, 0);
 
         public ExcludedBmpRegion(Bitmap remaining)
         {
-            Remaining = remaining;
+            Remaining = new Bitmap(remaining);
         }
 
         public void Dispose()
         {
-            if(this.Remaining != null)
+            if (this.Remaining != null)
                 this.Remaining.Dispose();
+            this.Remaining = null;
         }
     }
 }
